69d32be298bb2c8ad3d81265132fc9f6005e0c3e
abdullah <abdullasif1>
2023-01-12 03:30:38 +0500

    Conditionally run effects(dependencies added)

--- src/Components/UseEffectOne.js
+++ src/Components/UseEffectOne.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react'
 
 function UseEffectOne() {
   const [count, setCount] = useState(0)
-
+  const [name, setName] = useState('')
   useEffect(() => {
+    console.log('updating')
     document.title = `Clicked ${count} times`
-  })
+  },[count])
   
   return (
     <div>
+      <input type='text' value ={name} onChange={e => setName(e.target.value)}/>
       <button className='btn btn-primary' onClick={()=> setCount(count+1)}>Clicked {count} times</button>
     </div>
   )
